09d2e88dd2c1e62bd2e8a55b5f9674402e6e8c04
sameeranwarr <sameer.duke@gmail.com>
2024-09-14 17:21:20 +0500

    images.py
    Python code to load image

--- Files/images.py
+++ Files/images.py
@@ -1,1 +1,8 @@
-
+import cv2
+image = cv2.imread('../Images/sample.jpg') 
+if image is None:
+    print("Error: Could not load image.")
+else:
+    cv2.imshow('Loaded Image', image)
+    cv2.waitKey(0)  
+    cv2.destroyAllWindows()  
