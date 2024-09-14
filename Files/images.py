41b4b952bc4db450b611aa28a5f8c682ef4bcba4
sameeranwarr <sameer.duke@gmail.com>
2024-09-14 18:05:24 +0500

    images.py

--- Files/images.py
+++ Files/images.py
@@ -1,8 +1,8 @@
 import cv2
-image = cv2.imread('../Images/sample.jpg') 
+image = cv2.imread('bmw1.jpg') 
 if image is None:
-    print("Error: Could not load image.")
+    print("There is no Image!")
 else:
-    cv2.imshow('Loaded Image', image)
+    cv2.imshow('Image is loaded!', image)
     cv2.waitKey(0)  
     cv2.destroyAllWindows()  
